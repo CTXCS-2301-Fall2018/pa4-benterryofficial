0fac6dcf17bd8708e9e06b29355bd59ec8d0a729
Ben <benjamin.terry@ctx.edu>
2018-10-24 05:51:25 +0000

    PA4-1 completed

--- PA4-1.s
+++ PA4-1.s
@@ -1,5 +1,6 @@
-
-	@	Put the required header information here
+@ Ben Terry
+@ Programming Assignment 4 - Sums integers between two values
+@ 23 October 2018
 
 	.extern printf
 	.extern scanf
@@ -17,15 +18,22 @@ main:
 	@ in R1.
 	@-------------
 
-	
-	
+	MOV r1, #0	@ r1 = 0
+_loop:
+	CMP r3, r2	@ Compare if r3 = r2
+	ADD r1, r1, r2	@ Add r1 + r2, store in r1
+	ADD r2, r2, r2	@ Add r2 + r2, store in r2
+	BGT _loop	@ Branch if greater than
+
+
+
 	@-------------
 	@ Do NOT alter code past this line.
 	@-------------
 	LDR	R0, =out
 	BL	printf
 	POP	{PC}
-	
+
 .data
 startval:	.word 1
 endval:		.word 5
